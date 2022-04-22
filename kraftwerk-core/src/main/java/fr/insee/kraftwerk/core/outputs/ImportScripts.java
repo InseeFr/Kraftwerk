a7fde6425f236bfbe645d16286804ffa00c03bdf
Loïc Laporte <loic.laporte@insee.fr>
2022-04-22 16:21:33 +0200

    Update ImportScripts.java

--- kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/outputs/ImportScripts.java
+++ kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/outputs/ImportScripts.java
@@ -100,46 +100,39 @@ public class ImportScripts {
 
 			// PROC IMPORT
 			// careful about special characters, which can't be imported in SAS
-			script.append(String.format("PROC IMPORT datafile=%s \n",
-					tableName.substring(0, Math.min(tableName.length(), 6))));
-			script.append(String.format("out=work.%s  dbms=dlm replace; \n", tableName));
-			script.append(String.format("FORMAT "));
+			script.append("data WORK.DSN_TEMP; \n");
+			script.append("%let _EFIERR_ = 0; /* set the ERROR detection macro variable */ \n");
+			script.append(String.format("infile RACINE delimiter=\"%s\" MISSOVER DSD lrecl=13106 firstobs=2;\n",
+					Constants.CSV_OUTPUTS_SEPARATOR));
+
 			// Special treatment to display the length of the variables
 			Map<String, VariablesMap> metadataVariables = tableScriptInfo.getMetadataVariables();
 			Map<String, Variable> listVariables = tableScriptInfo.getAllLength(tableScriptInfo.getDataStructure(),
 					metadataVariables);
-			int count = 0;
-			for (String variableName : listVariables.keySet()) {
-				Variable variable = listVariables.get(variableName);
-				String length = variable.getLength();
-				if (!length.contentEquals("0")) {
-					// We write the format instructions if we have inrofmations on the length of the variables
-					if (variable.getType().equals(VariableType.BOOLEAN)
-							|| variable.getType().equals(VariableType.STRING)
-							|| variable.getType().equals(VariableType.DATE)) {
-						script.append(String.format("%s $%s. ", variableName, length));
-					} else if (variable.getType().equals(VariableType.INTEGER)
-							|| variable.getType().equals(VariableType.NUMBER)) {
-						if (length.contains(".")) {
-							script.append(String.format("%s %s ", variableName, length));
-						} else {
-							script.append(String.format("%s %s. ", variableName, length));
-						}
-					}
-					// SAS doesn't allow more than 6000 characters for each line, we do a break
-					// every once in a while
-					if (count < 40) {
-						count++;
-					} else {
-						script.append(String.format("\n"));
-						count = 0;
-					}
-				}
-			}
-			script.append(String.format("; \n"));
-			script.append(String.format("delimiter=\"%s\";\n", Constants.CSV_OUTPUTS_SEPARATOR));
-			script.append("getnames=yes; \n");
-			script.append("run;");
+
+			/*
+			 * informat MAA2AT $1. ; informat ANNEENQ best32. ;
+			 */
+
+			script.append(scriptSASPart1(listVariables));
+			script.append(String.format("\n"));
+
+			/*
+			 * format MAA2AT $1. ; format ANNEENQ best32. ;
+			 */
+
+			script.append(scriptSASPart2(listVariables));
+			script.append(String.format("\n"));
+
+			/*
+			 * input IdUE $ ADRESSE $ NHAB MAA2AT $;
+			 */
+
+			script.append(scriptSASPart3(listVariables));
+			script.append(String.format("\n"));
+
+			script.append("if _ERROR_ then call symputx('_EFIERR_',1);\n");
+			script.append("run;\n");
 
 			script.append("\n\n");
 		}
@@ -170,5 +163,128 @@ public class ImportScripts {
 			log.debug("Missing variable type: this should not happen!");
 			return null;
 		}
+	}
+
+	/*
+	 * informat MAA2AT $1. ; informat ANNEENQ best32. ;
+	 */
+
+	public String scriptSASPart1(Map<String, Variable> listVariables) {
+
+		StringBuilder script = new StringBuilder();
+		for (String variableName : listVariables.keySet()) {
+			Variable variable = listVariables.get(variableName);
+			String length = variable.getLength();
+			if (!length.contentEquals("0")) {
+				// We write the format instructions if we have inrofmations on the length of the
+				// variables
+				if (variable.getType().equals(VariableType.BOOLEAN) || variable.getType().equals(VariableType.STRING)
+						|| variable.getType().equals(VariableType.DATE)) {
+					script.append(String.format("informat %s $%s. ;\n", variableName, length));
+				} else if (variable.getType().equals(VariableType.INTEGER)
+						|| variable.getType().equals(VariableType.NUMBER)) {
+					if (length.contains(".")) {
+						script.append(String.format("informat %s best32. ;\n", variableName, length));
+					} else {
+						script.append(String.format("informat %s best32. ;\n", variableName, length));
+					}
+				}
+
+			} else {
+				if (variable.getType().equals(VariableType.BOOLEAN) || variable.getType().equals(VariableType.STRING)
+						|| variable.getType().equals(VariableType.DATE)) {
+					script.append(String.format("informat %s $32. ;\n", variableName));
+				} else if (variable.getType().equals(VariableType.INTEGER)
+						|| variable.getType().equals(VariableType.NUMBER)) {
+					script.append(String.format("informat %s best32. ;\n", variableName, length));
+				}
+			}
+
+		}
+
+		script.append(";\n\n");
+		return script.toString();
+	}
+
+	/*
+	 * format MAA2AT $1. ; format ANNEENQ best32. ;
+	 */
+
+	public String scriptSASPart2(Map<String, Variable> listVariables) {
+
+		StringBuilder script = new StringBuilder();
+		for (String variableName : listVariables.keySet()) {
+			Variable variable = listVariables.get(variableName);
+			String length = variable.getLength();
+			if (!length.contentEquals("0")) {
+				// We write the format instructions if we have inrofmations on the length of the
+				// variables
+				if (variable.getType().equals(VariableType.BOOLEAN) || variable.getType().equals(VariableType.STRING)
+						|| variable.getType().equals(VariableType.DATE)) {
+					script.append(String.format("format %s $%s. ;\n", variableName, length));
+				} else if (variable.getType().equals(VariableType.INTEGER)
+						|| variable.getType().equals(VariableType.NUMBER)) {
+					if (length.contains(".")) {
+						script.append(String.format("format %s best32. ;\n", variableName, length));
+					} else {
+						script.append(String.format("format %s best32. ;\n", variableName, length));
+					}
+				}
+
+			} else {
+				if (variable.getType().equals(VariableType.BOOLEAN) || variable.getType().equals(VariableType.STRING)
+						|| variable.getType().equals(VariableType.DATE)) {
+					script.append(String.format("format %s $32. ;\n", variableName));
+				} else if (variable.getType().equals(VariableType.INTEGER)
+						|| variable.getType().equals(VariableType.NUMBER)) {
+					script.append(String.format("format %s best32. ;\n", variableName, length));
+				}
+			}
+
+		}
+
+		script.append(";\n\n");
+		return script.toString();
+	}
+
+	/*
+	 * input IdUE $ ADRESSE $ NHAB MAA2AT $;
+	 */
+	public String scriptSASPart3(Map<String, Variable> listVariables) {
+
+		StringBuilder script = new StringBuilder();
+
+		int count = 0;
+		script.append(String.format("input \n "));
+		for (String variableName : listVariables.keySet()) {
+			Variable variable = listVariables.get(variableName);
+			String length = variable.getLength();
+
+			if (!length.contentEquals("0")) {
+				// We write the format instructions if we have informations on the length of the
+				// variables
+				if (variable.getType().equals(VariableType.BOOLEAN) || variable.getType().equals(VariableType.STRING)
+						|| variable.getType().equals(VariableType.DATE)) {
+					script.append(String.format("%s $ \n", variableName, length));
+				} else if (variable.getType().equals(VariableType.INTEGER)
+						|| variable.getType().equals(VariableType.NUMBER)) {
+
+					script.append(String.format("%s \n", variableName, length));
+
+				}
+				// SAS doesn't allow more than 6000 characters for each line, we do a break
+				// every once in a while
+				if (count < 40) {
+					count++;
+				} else {
+					script.append(String.format("\n"));
+					count = 0;
+				}
+			} else {
+
+			}
+		}
+		script.append(";\n");
+		return script.toString();
 	}
 }
