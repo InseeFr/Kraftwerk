2df92a78ef574dd785bb7e90b7261a691785f702
nsenave <nicolas.senave@gmail.com>
2022-04-26 10:55:01 +0200

    add catch for antlr exception

--- kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/vtl/VtlBindings.java
+++ kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/vtl/VtlBindings.java
@@ -167,6 +167,10 @@ public class VtlBindings {
                 log.debug("NullPointerException thrown when trying to evaluate following expression:\n" + vtlScript);
                 log.debug(e.getMessage());
                 log.debug("Probable cause: one of the operator used not yet supported by Trevas java library.");
+            } catch (Error e) {
+                log.debug("Error thrown when trying to evaluate following expression:\n" + vtlScript);
+                log.error(e.getMessage());
+                log.error("Probable cause: Syntax error.");
             }
         } else {
             log.info("null or empty VTL instruction given. VTL bindings has not been changed.");
