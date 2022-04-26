d2a28a1ca38e16fc8d3264d1ffe8051b976008fb
nsenave <nicolas.senave@gmail.com>
2022-04-26 11:21:42 +0200

    move down method

--- kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/metadata/VariablesMap.java
+++ kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/metadata/VariablesMap.java
@@ -53,10 +53,7 @@ public class VariablesMap {
     public Set<String> getVariableNames() {
         return variables.keySet();
     }
-    /** Return the fully qualified names of all variables in the map. */
-    public Set<String> getFullyQualifiedNames() {
-        return variables.keySet().stream().map(this::getFullyQualifiedName).collect(Collectors.toSet());
-    }
+
 
     /** Return the number of all variables registered in the map. */
     public int getVariablesCount() {
@@ -147,6 +144,11 @@ public class VariablesMap {
                     variableName));
             return null;
         }
+    }
+
+    /** Return the fully qualified names of all variables in the map. */
+    public Set<String> getFullyQualifiedNames() {
+        return variables.keySet().stream().map(this::getFullyQualifiedName).collect(Collectors.toSet());
     }
 
     /** Return the variables names that belongs to the group. */
