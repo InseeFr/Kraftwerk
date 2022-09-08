bdcbd1fc19a2edb5cb5d38afb2bfdb15355f675a
Alice Lambois <alice.lambois@insee.fr>
2022-09-08 16:21:59 +0200

    Update ParaDataUE.java

--- kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/extradata/paradata/ParaDataUE.java
+++ kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/extradata/paradata/ParaDataUE.java
@@ -6,7 +6,6 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class ParaDataUE {
