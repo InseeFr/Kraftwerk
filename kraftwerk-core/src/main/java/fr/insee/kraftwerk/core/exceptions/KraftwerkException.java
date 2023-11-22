a71911b525bcde3b8be1bc5941fd4f8fcb810333
nsenave <nicolas.senave@gmail.com>
2023-11-23 00:23:52 +0100

    refactor: add serial annotation in exception

--- kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/exceptions/KraftwerkException.java
+++ kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/exceptions/KraftwerkException.java
@@ -3,9 +3,12 @@ package fr.insee.kraftwerk.core.exceptions;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.io.Serial;
+
 @AllArgsConstructor
 public class KraftwerkException extends Exception {
 
+	@Serial
 	private static final long serialVersionUID = -7959158367542389147L;
 
 	@Getter
