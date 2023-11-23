bf764bc2bb7cacde0856ef29f2af8baa9f8b2249
QDIBYS <alexis.szmundy@insee.fr>
2023-11-23 15:34:01 +0100

    Add paradata sampletest

---
+++ kraftwerk-functional-tests/src/test/java/cucumber/TestConstants.java
@@ -0,0 +1,15 @@
+package cucumber;
+
+/**
+ * Class to create static variables giving path for test resources.
+ */
+public class TestConstants {
+
+	public static final String TEST_RESOURCES_DIRECTORY = "src/test/resources";
+
+	//Functional tests
+	public static final String FUNCTIONAL_TESTS_INPUT_DIRECTORY = TEST_RESOURCES_DIRECTORY + "/functional_tests/in";
+	public static final String FUNCTIONAL_TESTS_TEMP_DIRECTORY = TEST_RESOURCES_DIRECTORY + "/functional_tests/temp";
+	public static final String FUNCTIONAL_TESTS_OUTPUT_DIRECTORY = TEST_RESOURCES_DIRECTORY + "/functional_tests/out";
+
+}
