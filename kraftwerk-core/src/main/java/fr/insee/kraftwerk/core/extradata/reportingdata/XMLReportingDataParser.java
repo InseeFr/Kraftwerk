b2af427cf25a17ef9b5c001d4bcb3dc09d5c82f1
QDIBYS <alexis.szmundy@insee.fr>
2023-12-14 14:37:42 +0100

    Refactors on XML Reporting Data Parser

--- kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/extradata/reportingdata/XMLReportingDataParser.java
+++ kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/extradata/reportingdata/XMLReportingDataParser.java
@@ -38,28 +38,24 @@ public class XMLReportingDataParser extends ReportingDataParser {
       Element identifierElement = surveyUnitElement.getFirstChildElement("Id");
       String identifier = identifierElement.getValue();
       reportingDataUE.setIdentifier(identifier);
+
       Element interviewerIdentifierElement = surveyUnitElement.getFirstChildElement("InterviewerId");
       String interviewerIdentifier = Constants.REPORTING_DATA_INTERVIEWER_ID_NULL_PLACEHOLDER + identifier;
       if(interviewerIdentifierElement != null){
         interviewerIdentifier = interviewerIdentifierElement.getValue();
       }
       reportingDataUE.setInterviewerId(interviewerIdentifier);
+
       Element organizationalUnitIdentifierElement = surveyUnitElement
         .getFirstChildElement("OrganizationalUnitId");
       if(organizationalUnitIdentifierElement != null) {
-        // Get address values
         String organizationalUnitIdentifier = organizationalUnitIdentifierElement.getValue();
         reportingDataUE.setOrganizationUnitId(organizationalUnitIdentifier);
-        Element adressElement = surveyUnitElement.getFirstChildElement("InseeSampleIdentiers");
-        reportingDataUE.setInseeSampleIdentifier(new InseeSampleIdentifier());
-        reportingDataUE.getInseeSampleIdentifier().setRges(adressElement.getFirstChildElement("Rges").getValue());
-        reportingDataUE.getInseeSampleIdentifier().setNumfa(adressElement.getFirstChildElement("Numfa").getValue());
-        reportingDataUE.getInseeSampleIdentifier().setSsech(adressElement.getFirstChildElement("Ssech").getValue());
-        reportingDataUE.getInseeSampleIdentifier().setLe(adressElement.getFirstChildElement("Le").getValue());
-        reportingDataUE.getInseeSampleIdentifier().setEc(adressElement.getFirstChildElement("Ec").getValue());
-        reportingDataUE.getInseeSampleIdentifier().setBs(adressElement.getFirstChildElement("Bs").getValue());
-        reportingDataUE.getInseeSampleIdentifier().setNoi(adressElement.getFirstChildElement("Noi").getValue());
       }
+
+      // Get address values
+      getAddress(surveyUnitElement, reportingDataUE);
+
       Elements stateNodeList = surveyUnitElement.getFirstChildElement("States").getChildElements("State");
       for (int j = 0; j < stateNodeList.size(); j++) {
         Element stateElement = stateNodeList.get(j);
@@ -68,6 +64,7 @@ public class XMLReportingDataParser extends ReportingDataParser {
         reportingDataUE.addState(new State(type, Long.parseLong(timestamp)));
       }
       reportingDataUE.sortStates();
+
       // Get outcome values
       Element contactOutcomeElement = surveyUnitElement.getFirstChildElement("ContactOutcome");
       reportingDataUE.setContactOutcome(new ContactOutcome());
@@ -79,6 +76,7 @@ public class XMLReportingDataParser extends ReportingDataParser {
         reportingDataUE.getContactOutcome().setTotalNumberOfContactAttempts(
             Integer.parseInt(contactOutcomeElement.getFirstChildElement("totalNumberOfContactAttempts").getValue()));
       }
+
       Element contactAttemptsNode = surveyUnitElement.getFirstChildElement("ContactAttempts");
       if(contactAttemptsNode != null) {
         Elements contactAttemptsElements = contactAttemptsNode.getChildElements("ContactAttempt");
@@ -91,44 +89,18 @@ public class XMLReportingDataParser extends ReportingDataParser {
       }
 
         //Get identification
-      Element identificationElement = surveyUnitElement.getFirstChildElement("Identification");
-      reportingDataUE.setIdentification(new Identification());
-      if (identificationElement != null) {
-        if (identificationElement.getFirstChildElement(Constants.IDENTIFICATION_NAME) != null) {
-          reportingDataUE.getIdentification().setIdentification(identificationElement.getFirstChildElement(Constants.IDENTIFICATION_NAME).getValue());
-        }
-        if (identificationElement.getFirstChildElement(Constants.ACCESS_NAME) != null) {
-          reportingDataUE.getIdentification().setAccess(identificationElement.getFirstChildElement(Constants.ACCESS_NAME).getValue());
-        }
-        if (identificationElement.getFirstChildElement(Constants.SITUATION_NAME) != null) {
-          reportingDataUE.getIdentification().setSituation(identificationElement.getFirstChildElement(Constants.SITUATION_NAME).getValue());
-        }
-        if (identificationElement.getFirstChildElement(Constants.CATEGORY_NAME) != null) {
-          reportingDataUE.getIdentification().setCategory(identificationElement.getFirstChildElement(Constants.CATEGORY_NAME).getValue());
-        }
-        if (identificationElement.getFirstChildElement(Constants.OCCUPANT_NAME) != null) {
-          reportingDataUE.getIdentification().setOccupant(identificationElement.getFirstChildElement(Constants.OCCUPANT_NAME).getValue());
-        }
-      }
+      getIdentification(surveyUnitElement,reportingDataUE);
 
       //Get comments
-      Element commentsNode = surveyUnitElement.getFirstChildElement("Comments");
-      if(commentsNode != null) {
-        Elements commentsElements = commentsNode.getChildElements("Comment");
-        for (int k = 0; k < commentsElements.size(); k++) {
-          Element commentsElement = commentsElements.get(k);
-          String status = commentsElement.getFirstChildElement("type").getValue();
-          String value = commentsElement.getFirstChildElement("value").getValue();
-          reportingDataUE.addComment(new Comment(status, value));
-        }
-      }
+      getComments(surveyUnitElement, reportingDataUE);
 
-        reportingData.addReportingDataUE(reportingDataUE);
+      reportingData.addReportingDataUE(reportingDataUE);
     }
     integrateReportingDataIntoUE(data, reportingData, withAllReportingData);
     this.document = null;
   }
-  
+
+
   private void readFile(Path filePath) {
     XmlFileReader xmlFileReader = new XmlFileReader();
     this.document = xmlFileReader.readXmlFile(filePath);
@@ -159,4 +131,54 @@ public class XMLReportingDataParser extends ReportingDataParser {
 
     return surveyUnitList;
   }
+
+  private void getAddress(Element surveyUnitElement, ReportingDataUE reportingDataUE) {
+    Element addressElement = surveyUnitElement.getFirstChildElement("InseeSampleIdentiers");
+
+    if(addressElement != null) {
+      reportingDataUE.setInseeSampleIdentifier(new InseeSampleIdentifier());
+      reportingDataUE.getInseeSampleIdentifier().setRges(addressElement.getFirstChildElement("Rges").getValue());
+      reportingDataUE.getInseeSampleIdentifier().setNumfa(addressElement.getFirstChildElement("Numfa").getValue());
+      reportingDataUE.getInseeSampleIdentifier().setSsech(addressElement.getFirstChildElement("Ssech").getValue());
+      reportingDataUE.getInseeSampleIdentifier().setLe(addressElement.getFirstChildElement("Le").getValue());
+      reportingDataUE.getInseeSampleIdentifier().setEc(addressElement.getFirstChildElement("Ec").getValue());
+      reportingDataUE.getInseeSampleIdentifier().setBs(addressElement.getFirstChildElement("Bs").getValue());
+      reportingDataUE.getInseeSampleIdentifier().setNoi(addressElement.getFirstChildElement("Noi").getValue());
+    }
+  }
+
+  private void getIdentification(Element surveyUnitElement, ReportingDataUE reportingDataUE) {
+    Element identificationElement = surveyUnitElement.getFirstChildElement("Identification");
+    reportingDataUE.setIdentification(new Identification());
+    if (identificationElement != null) {
+      if (identificationElement.getFirstChildElement(Constants.IDENTIFICATION_NAME) != null)
+        reportingDataUE.getIdentification().setIdentification(identificationElement.getFirstChildElement(Constants.IDENTIFICATION_NAME).getValue());
+
+      if (identificationElement.getFirstChildElement(Constants.ACCESS_NAME) != null)
+        reportingDataUE.getIdentification().setAccess(identificationElement.getFirstChildElement(Constants.ACCESS_NAME).getValue());
+
+      if (identificationElement.getFirstChildElement(Constants.SITUATION_NAME) != null)
+        reportingDataUE.getIdentification().setSituation(identificationElement.getFirstChildElement(Constants.SITUATION_NAME).getValue());
+
+      if (identificationElement.getFirstChildElement(Constants.CATEGORY_NAME) != null)
+        reportingDataUE.getIdentification().setCategory(identificationElement.getFirstChildElement(Constants.CATEGORY_NAME).getValue());
+
+      if (identificationElement.getFirstChildElement(Constants.OCCUPANT_NAME) != null)
+        reportingDataUE.getIdentification().setOccupant(identificationElement.getFirstChildElement(Constants.OCCUPANT_NAME).getValue());
+    }
+  }
+
+  private void getComments(Element surveyUnitElement, ReportingDataUE reportingDataUE) {
+    Element commentsNode = surveyUnitElement.getFirstChildElement("Comments");
+    if(commentsNode != null) {
+      Elements commentsElements = commentsNode.getChildElements("Comment");
+      for (int k = 0; k < commentsElements.size(); k++) {
+        Element commentsElement = commentsElements.get(k);
+        String status = commentsElement.getFirstChildElement("type").getValue();
+        String value = commentsElement.getFirstChildElement("value").getValue();
+        reportingDataUE.addComment(new Comment(status, value));
+      }
+    }
+  }
+
 }
