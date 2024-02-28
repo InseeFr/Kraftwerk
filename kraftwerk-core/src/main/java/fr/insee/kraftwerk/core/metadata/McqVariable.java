f04ef9b5085c5dcfecfbfe5e9529baf5d356f70f
Alice Lambois <alice.lambois@insee.fr>
2024-02-28 13:38:11 +0100

    Update McqVariable.java

--- kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/metadata/McqVariable.java
+++ kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/metadata/McqVariable.java
@@ -8,13 +8,14 @@ import lombok.extern.log4j.Log4j2;
 /**
  * One multiple choice question with K modalities = K MCQ Variables.
  */
+
+@Setter
+@Getter
 @Log4j2
 public class McqVariable extends Variable {
 
 	/** Text associated with the modality. */
-	@Getter
-	@Setter
-	String text;
+	private String text;
 
 	public McqVariable(String name, Group group, VariableType type) {
 		super(name, group, type);
