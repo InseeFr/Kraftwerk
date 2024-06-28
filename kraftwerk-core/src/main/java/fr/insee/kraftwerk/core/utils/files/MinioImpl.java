53dfbe54e82439cfa81368f76195d2f4b939de33
QDIBYS <alexis.szmundy@insee.fr>
2024-06-28 11:40:11 +0200

    Update MinioImpl.java

--- kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/utils/files/MinioImpl.java
+++ kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/utils/files/MinioImpl.java
@@ -21,13 +21,11 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
