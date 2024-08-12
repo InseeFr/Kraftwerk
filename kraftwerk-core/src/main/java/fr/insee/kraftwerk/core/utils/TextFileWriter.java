eb92cbbc23821646790d554310146f561aa2f412
QDIBYS <alexis.szmundy@insee.fr>
2024-08-12 14:28:01 +0200

    remove unused import

--- kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/utils/TextFileWriter.java
+++ kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/utils/TextFileWriter.java
@@ -2,7 +2,6 @@ package fr.insee.kraftwerk.core.utils;
 
 import java.nio.file.Path;
 import java.time.LocalDateTime;
-import java.util.List;
 
 import fr.insee.kraftwerk.core.KraftwerkError;
 import fr.insee.kraftwerk.core.Constants;
