a30a3c83594aa8d5e9ebe40a48ea47a8bfa6736a
QDIBYS <alexis.szmundy@insee.fr>
2024-11-12 10:56:01 +0100

    Update MainService.java

--- kraftwerk-api/src/main/java/fr/insee/kraftwerk/api/services/MainService.java
+++ kraftwerk-api/src/main/java/fr/insee/kraftwerk/api/services/MainService.java
@@ -60,19 +60,7 @@ public class MainService extends KraftwerkService {
 			) {
 		boolean fileByFile = false;
 		boolean withDDI = true;
-		FileUtilsInterface fileUtilsInterface = getFileUtilsInterface();
-
-		MainProcessing mp = new MainProcessing(inDirectoryParam, fileByFile,withAllReportingData,withDDI, defaultDirectory, limitSize, fileUtilsInterface);
-		try {
-			mp.runMain();
-		} catch (KraftwerkException e) {
-			return ResponseEntity.status(e.getStatus()).body(e.getMessage());
-		}
-
-		/* Step 4.3- 4.4 : Archive */
-		if (Boolean.TRUE.equals(archiveAtEnd)) archive(inDirectoryParam,fileUtilsInterface);
-
-		return ResponseEntity.ok(inDirectoryParam);
+		return runWithoutGenesis(inDirectoryParam, archiveAtEnd, fileByFile, withAllReportingData, withDDI);
 	}
 
 	@PutMapping(value = "/main/file-by-file")
@@ -84,18 +72,7 @@ public class MainService extends KraftwerkService {
 		boolean fileByFile = true;
 		boolean withAllReportingData = false;
 		boolean withDDI = true;
-		FileUtilsInterface fileUtilsInterface = getFileUtilsInterface();
-
-		MainProcessing mp = new MainProcessing(inDirectoryParam, fileByFile,withAllReportingData,withDDI, defaultDirectory, limitSize, fileUtilsInterface);
-		try {
-			mp.runMain();
-		} catch (KraftwerkException e) {
-			return ResponseEntity.status(e.getStatus()).body(e.getMessage());
-		}
-			/* Step 4.3- 4.4 : Archive */
-		if (Boolean.TRUE.equals(archiveAtEnd)) archive(inDirectoryParam, fileUtilsInterface);
-
-		return ResponseEntity.ok(inDirectoryParam);
+		return runWithoutGenesis(inDirectoryParam, archiveAtEnd, fileByFile, withAllReportingData, withDDI);
 	}
 
 	@PutMapping(value = "/main/lunatic-only")
@@ -107,36 +84,15 @@ public class MainService extends KraftwerkService {
 		boolean withDDI = false;
 		boolean fileByFile = false;
 		boolean withAllReportingData = false;
-		FileUtilsInterface fileUtilsInterface = getFileUtilsInterface();
-
-		MainProcessing mp = new MainProcessing(inDirectoryParam, fileByFile,withAllReportingData,withDDI, defaultDirectory, limitSize, fileUtilsInterface);
-		try {
-			mp.runMain();
-		} catch (KraftwerkException e) {
-			return ResponseEntity.status(e.getStatus()).body(e.getMessage());
-		}
-			/* Step 4.3- 4.4 : Archive */
-		if (Boolean.TRUE.equals(archiveAtEnd)) archive(inDirectoryParam, fileUtilsInterface);
-
-		return ResponseEntity.ok(inDirectoryParam);
+		return runWithoutGenesis(inDirectoryParam, archiveAtEnd, fileByFile, withAllReportingData, withDDI);
 	}
 
 	@PutMapping(value = "/main/genesis")
 	@Operation(operationId = "mainGenesis", summary = "${summary.mainGenesis}", description = "${description.mainGenesis}")
 	public ResponseEntity<String> mainGenesis(
 			@Parameter(description = "${param.idCampaign}", required = true, example = INDIRECTORY_EXAMPLE) @RequestBody String idCampaign) {
-		FileUtilsInterface fileUtilsInterface = getFileUtilsInterface();
-
-		MainProcessingGenesis mpGenesis = new MainProcessingGenesis(configProperties, fileUtilsInterface);
-
-		try {
-			mpGenesis.runMain(idCampaign);
-		} catch (KraftwerkException e) {
-			return ResponseEntity.status(e.getStatus()).body(e.getMessage());
-		} catch (IOException e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-		}
-		return ResponseEntity.ok(idCampaign);
+		boolean withDDI = true;
+		return runWithGenesis(idCampaign, withDDI);
 	}
 
 
@@ -145,6 +101,27 @@ public class MainService extends KraftwerkService {
 	public ResponseEntity<String> mainGenesisLunaticOnly(
 			@Parameter(description = "${param.idCampaign}", required = true, example = INDIRECTORY_EXAMPLE) @RequestBody String idCampaign) {
 		boolean withDDI = false;
+		return runWithGenesis(idCampaign, withDDI);
+	}
+
+	@NotNull
+	private ResponseEntity<String> runWithoutGenesis(String inDirectoryParam, boolean archiveAtEnd, boolean fileByFile, boolean withAllReportingData, boolean withDDI) {
+		FileUtilsInterface fileUtilsInterface = getFileUtilsInterface();
+
+		MainProcessing mp = new MainProcessing(inDirectoryParam, fileByFile, withAllReportingData, withDDI, defaultDirectory, limitSize, fileUtilsInterface);
+		try {
+			mp.runMain();
+		} catch (KraftwerkException e) {
+			return ResponseEntity.status(e.getStatus()).body(e.getMessage());
+		}
+		/* Step 4.3- 4.4 : Archive */
+		if (Boolean.TRUE.equals(archiveAtEnd)) archive(inDirectoryParam, fileUtilsInterface);
+
+		return ResponseEntity.ok(inDirectoryParam);
+	}
+
+	@NotNull
+	private ResponseEntity<String> runWithGenesis(String idCampaign, boolean withDDI) {
 		FileUtilsInterface fileUtilsInterface = getFileUtilsInterface();
 
 		MainProcessingGenesis mpGenesis = new MainProcessingGenesis(configProperties, fileUtilsInterface, withDDI);
@@ -168,5 +145,4 @@ public class MainService extends KraftwerkService {
 		}
 		return fileUtilsInterface;
 	}
-
 }
