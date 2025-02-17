52b557f48d35cc9047171ab3fa9991dcad1637fa
Loïc Henninger <loic.henninger@insee.fr>
2025-02-17 16:57:02 +0100

    fix: cucumber tests

--- kraftwerk-core/src/test/java/RunCucumberTest.java
+++ kraftwerk-core/src/test/java/RunCucumberTest.java
@@ -4,6 +4,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"pretty"}, glue = {"cucumber.unit_tests"})
 public class RunCucumberTest {
 }
