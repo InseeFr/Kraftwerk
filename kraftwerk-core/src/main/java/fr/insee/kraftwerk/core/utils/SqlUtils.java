1bcb15ddb49f501198981bfdd2f061ce94058496
QDIBYS <alexis.szmundy@insee.fr>
2025-03-10 15:01:30 +0100

    fix: Missing variables in duckdb tables

--- kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/utils/SqlUtils.java
+++ kraftwerk-core/src/main/java/fr/insee/kraftwerk/core/utils/SqlUtils.java
@@ -56,7 +56,11 @@ public class SqlUtils {
      * @param sqlSchema schema of dataset
      * @throws SQLException if sql error
      */
-    private static void createDataSQLTables(Statement statement, String datasetName, LinkedHashMap<String, VariableType> sqlSchema) throws SQLException {
+    private static void createDataSQLTables(
+            Statement statement,
+            String datasetName,
+            LinkedHashMap<String, VariableType> sqlSchema
+    ) throws SQLException {
 
         //Skip if no variable
         if (sqlSchema.isEmpty()) {
@@ -64,7 +68,7 @@ public class SqlUtils {
             return;
         }
 
-        //Skip CREATE if table already exists (ex: file-by-file)
+        //Don't CREATE if table already exists (ex: file-by-file)
         List<String> tableNames = getTableNames(statement);
         if (!tableNames.contains(datasetName)) {
             String createTableQuery = getCreateTableQuery(datasetName, sqlSchema);
@@ -72,15 +76,23 @@ public class SqlUtils {
             //Execute query
             log.debug("SQL Query : {}", createTableQuery);
             statement.execute(createTableQuery);
+            return;
         }
+        //add missing columns if necessary
+        LinkedHashMap<String, VariableType> variablesToAdd = getVariablesToAdd(statement, datasetName, sqlSchema);
+        if(variablesToAdd.isEmpty()){
+            return;
+        }
+        String updateTableQuery = getUpdateTableQuery(datasetName, variablesToAdd);
+        statement.execute(updateTableQuery);
     }
 
-    private static String getCreateTableQuery(String datasetName, LinkedHashMap<String, VariableType> sqlSchema) {
+    private static String getCreateTableQuery(String datasetName, LinkedHashMap<String, VariableType> variablesToAdd) {
         //CREATE query building
         StringBuilder createTableQuery = new StringBuilder(String.format("CREATE TABLE \"%s\" (", datasetName));
 
-        for (Map.Entry<String, VariableType> column : sqlSchema.entrySet()) {
-            createTableQuery.append("\"").append(column.getKey()).append("\"").append(" ").append(sqlSchema.get(column.getKey()).getSqlType());
+        for (Map.Entry<String, VariableType> column : variablesToAdd.entrySet()) {
+            createTableQuery.append("\"").append(column.getKey()).append("\"").append(" ").append(variablesToAdd.get(column.getKey()).getSqlType());
             createTableQuery.append(", ");
         }
 
@@ -88,6 +100,36 @@ public class SqlUtils {
         createTableQuery.delete(createTableQuery.length() - 2, createTableQuery.length());
         createTableQuery.append(")");
         return createTableQuery.toString();
+    }
+
+    private static LinkedHashMap<String, VariableType> getVariablesToAdd(
+            Statement statement,
+            String datasetName,
+            LinkedHashMap<String, VariableType> sqlSchema
+    ) throws SQLException {
+        LinkedHashMap<String,VariableType> variablesToAdd = new LinkedHashMap<>();
+        List<String> columnsAlreadyInDatabase = getColumnNames(statement, datasetName);
+        //Filter out variable names already in database table
+        for(String variableNameToAdd :
+                sqlSchema.keySet().stream().filter(variableName -> !columnsAlreadyInDatabase.contains(variableName)).toList()){
+            variablesToAdd.put(variableNameToAdd,sqlSchema.get(variableNameToAdd));
+        }
+        return variablesToAdd;
+    }
+
+    private static String getUpdateTableQuery(String datasetName, LinkedHashMap<String, VariableType> variablesToAdd) {
+        //ALTER TABLE query building
+        StringBuilder alterTableQuery = new StringBuilder();
+
+        for (Map.Entry<String, VariableType> column : variablesToAdd.entrySet()) {
+            alterTableQuery.append(String.format("ALTER TABLE \"%s\" ADD COLUMN \"", datasetName)).append(column.getKey()).append("\"").append(" ").append(variablesToAdd.get(column.getKey()).getSqlType());
+            alterTableQuery.append("; ");
+        }
+
+        //Remove last delimiter and replace by ";"
+        alterTableQuery.delete(alterTableQuery.length() - 2, alterTableQuery.length());
+        alterTableQuery.append(";");
+        return alterTableQuery.toString();
     }
 
     /**
--- kraftwerk-functional-tests/src/test/resources/features/do_we_export_data_file_by_file.feature
+++ kraftwerk-functional-tests/src/test/resources/features/do_we_export_data_file_by_file.feature
@@ -1,11 +1,12 @@
 Feature: Do we save correctly all data file by file?
   Everybody wants to know if we save them correctly
 
-  Scenario Outline: Do we create CSV root data file with the right structure
+  Scenario Outline: Do we create root data file with the right structure
     Given Step 0 : We have some survey in directory "<Directory>"
     When Step 1 : We launch main service file by file
     Then Step 2 : We check root output file has <ExpectedLineCount> lines and <ExpectedDataFieldCount> variables
-    Then We check if the CSV format is correct
+    And We check if the CSV format is correct
+    And Step 2 : We check root parquet output file has <ExpectedLineCount> lines and <ExpectedDataFieldCount> variables
 
     Examples:
     # Parameters :
@@ -16,27 +17,14 @@ Feature: Do we save correctly all data file by file?
     |Directory                        |ExpectedLineCount     |ExpectedDataFieldCount            |
     |SAMPLETEST-MULTIPLEDATA-v1       |7                     |136                               |
     |SAMPLETEST-MULTIPLEDATA-v2       |4                     |136                               |
+    |SAMPLETEST-PARADATA-v1           |2                     |13                                |
 
-  Scenario Outline: Do we create Parquet root data file with the right structure
-    Given Step 0 : We have some survey in directory "<Directory>"
-    When Step 1 : We launch main service file by file
-    Then Step 2 : We check root parquet output file has <ExpectedLineCount> lines and <ExpectedDataFieldCount> variables
-
-    Examples:
-    # Parameters :
-    # - Directory : Directory of test campaigns
-    # - ExpectedLineCount : Expected row quantity
-    # - ExpectedDataFieldCount : Expected field quantity
-
-      |Directory                            |ExpectedLineCount     |ExpectedDataFieldCount            |
-      |SAMPLETEST-MULTIPLEDATA-v1           |6                     |136                               |
-      |SAMPLETEST-MULTIPLEDATA-v2           |3                     |136                               |
-
-  Scenario Outline: Do we create CSV loop data file with the right structure
+  Scenario Outline: Do we create loop data file with the right structure
     Given Step 0 : We have some survey in directory "<Directory>"
     When Step 1 : We launch main service file by file
     Then Step 2 : We check "<LoopName>" output file has <ExpectedLineCount> lines and <ExpectedDataFieldCount> variables
-    Then We check if the CSV format is correct
+    And We check if the CSV format is correct
+    And We check "<LoopName>" parquet output file has <ExpectedLineCount> lines and <ExpectedDataFieldCount> variables
 
     Examples:
     # Parameters :
@@ -49,20 +37,3 @@ Feature: Do we save correctly all data file by file?
       |SAMPLETEST-MULTIPLEDATA-v1       |BOUCLE_PRENOMS   |11                    |222                              |
       |SAMPLETEST-MULTIPLEDATA-v2       |BOUCLE_PRENOMS   |6                     |222                              |
       |SAMPLETEST-PARADATA-v1           |B_PRENOMREP      |3                     |1088                             |
-
-  Scenario Outline: Do we create Parquet loop data file with the right structure
-    Given Step 0 : We have some survey in directory "<Directory>"
-    When Step 1 : We launch main service file by file
-    Then We check "<LoopName>" parquet output file has <ExpectedLineCount> lines and <ExpectedDataFieldCount> variables
-
-    Examples:
-    # Parameters :
-    # - Directory : Directory of test campaigns
-    # - LoopName : Name of loop table
-    # - ExpectedLineCount : Expected row quantity
-    # - ExpectedDataFieldCount : Expected field quantity
-
-      |Directory                            |LoopName         |ExpectedLineCount     |ExpectedDataFieldCount        |
-      |SAMPLETEST-MULTIPLEDATA-v1           |BOUCLE_PRENOMS   |10                    |222                           |
-      |SAMPLETEST-MULTIPLEDATA-v2           |BOUCLE_PRENOMS   |5                     |222                           |
-      |SAMPLETEST-PARADATA-v1               |B_PRENOMREP      |2                     |1088                          |
