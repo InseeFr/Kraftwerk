fb6e4625f15ec8df8a71066c14c5030a5c6b0e3a
Hajarel-moukh <hajar.el-moukh@insee.fr>
2026-01-05 11:59:21 +0100

    correct CI

--- kraftwerk-encryption-tests/src/test/java/cucumber/functional_tests/MainDefinitions.java
+++ kraftwerk-encryption-tests/src/test/java/cucumber/functional_tests/MainDefinitions.java
@@ -644,7 +644,7 @@ public class MainDefinitions {
 		Path executionOutDirectory = outDirectory.resolve(Objects.requireNonNull(new File(outDirectory.toString()).listFiles(File::isDirectory))[0].getName());
 		CSVReader csvReader = getCSVReader(
 				executionOutDirectory.resolve(outDirectory.getFileName() + "_" + loopName + ".csv"));
-		// get header
+		// Get header
 		String[] header = csvReader.readNext();
 		//Assert fields existence
 		Assertions.assertThat(header).contains(Constants.ROOT_IDENTIFIER_NAME).contains(loopName).contains(fieldName);
